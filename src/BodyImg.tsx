2286120ccc809ecd9ac2845f1f4b0f8670b59597
Faari <faarijoye@gmail.com>
2024-01-24 23:20:52 +0100

    Initial commits

---
+++ src/BodyImg.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./BodyImg.css";
+
+const BodyImg = () => {
+  return (
+    <div className="main">
+      <img src="./bigshoes.png" alt="color" className="body-img" />
+      <div className="shoes">
+        <h1>AIRPRO X3</h1>
+        <h1>AIRPRO X3</h1>
+        <h1>AIRPRO X3</h1>
+      </div>
+    </div>
+  );
+};
+
+export default BodyImg;
---
+++ src/Navbar.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FaCartPlus, FaCaretDown } from "react-icons/fa";
+
+const Navbar = () => {
+  const [isDroppedDown, setIsDroppedDown] = useState(false);
+
+  const handleDown = () => {
+    setIsDroppedDown(!isDroppedDown);
+  };
+
+  return (
+    <div className="Navbar-class">
+      <a href="home">Home</a>
+      <a href="shop">Shop</a>
+      <div
+        onMouseEnter={handleDown}
+        onMouseLeave={handleDown}
+        style={{ marginTop: -3 }}
+      >
+        <a href="my account" style={{ paddingBottom: 10 }}>
+          My account <FaCaretDown />
+        </a>
+        {isDroppedDown && (
+          <div className="dropdown-menu">
+            <a href="orders">Orders</a>
+            <hr></hr>
+            <a href="address">Address</a>
+            <hr></hr>
+            <a href="a">Account details</a>
+          </div>
+        )}
+      </div>
+      <a href="about">About</a>
+      <a href="contact">Contact</a>
+      <div style={{ marginLeft: "100px" }}>
+        <span className="cart-price">$0.00</span>
+        <div className="notification-circle">0</div>
+        <FaCartPlus className="cart-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
